34d039048c167f569c50a537dac7777bea4eef2c
LikelyLime <qorwlgml2003@naver.com>
2025-01-21 13:27:53 +0900

    백준 유레카문제 10448 문제 단계별풀이

--- algorithm/algo11.java
+++ algorithm/algo11.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args){
+public class algo11 {
+    public static void main1(String[] args){
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
