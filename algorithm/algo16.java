5909adc3d862f0409ccfaffa7613246bcdc77912
LikelyLime <qorwlgml2003@naver.com>
2025-02-10 12:28:32 +0900

    백준 판화 1730 문제 단계별풀이

--- algorithm/algo16.java
+++ algorithm/algo16.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args){
+public class algo16 {
+    public static void main1(String[] args){
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
