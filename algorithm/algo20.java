6f8e83b1d9ef692d56fcfad3c589fe51d5aa33dc
LikelyLime <qorwlgml2003@naver.com>
2025-02-17 11:31:30 +0900

    백준 나이순정렬 10814 문제 단계별풀이

--- algorithm/algo20.java
+++ algorithm/algo20.java
@@ -1,9 +1,10 @@
 package algorithm;
 
-import java.util.*;
-import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
-public class Main {
+public class algo20 {
     static class Pepole{
         int age;
         String name;
@@ -31,7 +32,7 @@ public class Main {
             return idx - o.idx;
         }
     }
-    public static void main(String[] args){
+    public static void main1(String[] args){
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
