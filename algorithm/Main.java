6648d2e907208efbd1a987c2514d8c74533b4245
LikelyLime <qorwlgml2003@naver.com>
2025-03-04 12:36:29 +0900

    백준 2805 나무자르기

--- algorithm/Main.java
+++ algorithm/Main.java
@@ -7,50 +7,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] cards = new int[N];
-        for (int i = 0; i < N; i++) {
-            cards[i] = sc.nextInt();
-        }
-        Arrays.sort(cards);
         int M = sc.nextInt();
-        while (M-- > 0) {
-            int x = sc.nextInt();
-            int lowerBoundIndex = findLowerBoundIndex(cards, x);
-            int upperBoundIndex = findUpperBoundIndex(cards, x);
-            System.out.print(upperBoundIndex - lowerBoundIndex + " ");
+        Integer[] tree = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            tree[i] = sc.nextInt();
         }
-
-    }
-
-    private static int findUpperBoundIndex(int[] cards, int x) {
-        int upperBoundIndex = cards.length;
-        int l = 0, r = cards.length - 1;
+        int l = 0,r = 1000000000, ans = -1;
         while (l <= r) {
             int m = (l + r) / 2;
-            if (cards[m] <= x) {
+            if(isPossible(tree, m, M)){
+                ans = m;
                 l = m + 1;
             }else{
                 r = m - 1;
-                upperBoundIndex = m;
-
             }
         }
-        return upperBoundIndex;
+        System.out.println(ans);
     }
-    private static int findLowerBoundIndex(int[] cards, int x) {
-        int lowerBoundIndex = cards.length;
-        int l = 0, r = cards.length - 1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (cards[m] < x) {
-                l = m + 1;
-            }else{
-                r = m - 1;
-                lowerBoundIndex = m;
-
+    static boolean isPossible(Integer[] tree, int m, int M) {
+        long sum = 0;
+        for(int h : tree){
+            if(h > m){
+                sum += h - m;
             }
         }
-        return lowerBoundIndex;
+        return sum >= M;
     }
 }
 
